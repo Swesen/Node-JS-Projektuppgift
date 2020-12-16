f7953516bd2ecd47332598fdfcf12edf64f7db40
Swesen <38762425+Swesen@users.noreply.github.com>
2020-12-17 00:21:58 +0100

    First day

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+var webpagesFolder = __dirname + "/webpages";
+
 app.get("/", (req, res) => {
-    res.sendFile("index.html")
+    console.log(req.ip)
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(injectTopBar(fs.readFileSync(webpagesFolder + "/index.html")));
+    res.end();
 });
 
-app.listen(8080);
+function injectTopBar(page) {
+page = page.toString().split("%topbar%");
+return page[0] + fs.readFileSync(webpagesFolder + "/topbar.html") + page[1];
+}
+
+app.listen(8080, "0.0.0.0");
