1b9dc5e767e3254fd8688140c935de0cfd6f79ad
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-02-01 10:01:44 +0100

    Missed commit from end of project

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var fs = require("fs");
-const { hostname } = require("os");
 var url = require("url");
 var pageBuilder = require("./pageBuilder");
 
 var app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 var webPageNavbar = "./pageBlocks/navbar.html";
@@ -14,27 +13,27 @@ var webPageNavbar = "./pageBlocks/navbar.html";
 app.get("/*", (req, res) => {
     var urlPath = url.parse(req.url, true).pathname;
 
-    pageBuilder.addToHead('<meta charset="utf-8"/><link rel="stylesheet" href="/css/style.css"></link>');
+    pageBuilder.addToHead('<meta charset="utf-8"/><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="/css/style.css"></link>');
 
     pageBuilder.addBlockToBody(webPageNavbar);
 
     switch (urlPath) {
         case "/":
             pageBuilder.addToHead('<title>Länklådan</title><link rel="script" href="/JS/homed.js"/><link rel="icon" type="image/png" href="/favicon.png"/>');
-            pageBuilder.addBlockToBody("./pageBlocks/links.html")
+            pageBuilder.addBlockToBody(["./pageBlocks/links.html", "./pageBlocks/footerbar.html"])
             var linkList = fs.readFileSync("./link.lis").toString().split("\n");
-            var unorderedList = "<ul>"
+            var htmlList = "<ul>"
             for (let i = 0; i < linkList.length - 1; i++) {
                 var parsedLink = JSON.parse(linkList[i]);
-                unorderedList += '<li>' + '<a target="_blank" href="' + parsedLink.link + '">' + parsedLink.title + ' - <img src="http://' + url.parse(parsedLink.link).hostname + '/favicon.ico" alt="/link.png" onerror="this.src=this.alt">' +  '</a></li>';
+                htmlList += `<li><a target="_blank" href="${parsedLink.link}"><img src="https://${url.parse(parsedLink.link).hostname}/favicon.ico" alt="/link.png" onerror="this.src=this.alt"> - ${ parsedLink.title}</a></li>`;
             }
-            unorderedList += "</ul>"
-            pageBuilder.bodyInsertAtKey("%linkList%", unorderedList);
+            htmlList += "</ul>"
+            pageBuilder.bodyInsertAtKey("%linkList%", htmlList);
             break;
 
         case "/post":
             pageBuilder.addToHead('<title>Lägg till länk - Länklådan</title><link rel="script" href="/JS/post.js"></link>');
-            pageBuilder.addBlockToBody("./pageBlocks/postform.html")
+            pageBuilder.addBlockToBody(["./pageBlocks/postform.html", "./pageBlocks/footerbar.html"])
             break;
 
         default:
